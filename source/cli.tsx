a3ba3ccd5d62b0673e891717e8b31edbf2656fa8
Xpier0X <pieroleonyaipen07@gmail.com>
2026-03-04 21:12:02 -0500

    Inicial commit

---
+++ source/cli.tsx
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './app.js';
+
+const cli = meow(
+	`
+	Usage
+	  $ type-CLI
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ type-CLI --name=Jane
+	  Hello, Jane
+`,
+	{
+		importMeta: import.meta,
+		flags: {
+			name: {
+				type: 'string',
+			},
+		},
+	},
+);
+
+render(<App name={cli.flags.name} />);
